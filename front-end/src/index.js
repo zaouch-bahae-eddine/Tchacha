d608c91482fd893d2908ac52a3c53820dbd702b5
zaouch-bahae-eddine <zaouch.bahae.eddine@gmail.com>
2023-02-27 21:11:27 +0100

    fix api uri

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -10,7 +10,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/',
+  uri: 'https://chat-0rdz.onrender.com/',
 });
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('token');
